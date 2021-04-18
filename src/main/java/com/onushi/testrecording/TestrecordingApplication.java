a864552917c7e3a731cccc2ed0fcae7aae3643da
Ionut Breaz <ibreaz@yahoo.com>
2021-04-18 12:10:49 +0300

    create app

---
+++ src/main/java/com/onushi/testrecording/TestrecordingApplication.java
@@ -0,0 +1,13 @@
+package com.onushi.testrecording;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestrecordingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestrecordingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onushi/testrecording/TestrecordingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onushi.testrecording;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestrecordingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
