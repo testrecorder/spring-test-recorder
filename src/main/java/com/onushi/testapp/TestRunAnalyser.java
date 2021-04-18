4fa75684f7b863f82848d94836d85b4c8796789c
Ionut Breaz <ibreaz@yahoo.com>
2021-04-18 17:38:36 +0300

    Create TestRunAnalyser Extract interface for TestGenerator

---
+++ src/main/java/com/onushi/testapp/TestRunAnalyser.java
@@ -0,0 +1,41 @@
+package com.onushi.testapp;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.springframework.aop.aspectj.MethodInvocationProceedingJoinPoint;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+// TODO IB !!!! extract interface
+@Component
+public class TestRunAnalyser {
+
+    public TestRunDto createTestRunDto(ProceedingJoinPoint proceedingJoinPoint, Object result) {
+        MethodInvocationProceedingJoinPoint methodInvocation = (MethodInvocationProceedingJoinPoint)proceedingJoinPoint;
+
+        String packageAndClassName = proceedingJoinPoint.getSignature().getDeclaringTypeName();
+        String methodName = proceedingJoinPoint.getSignature().getName();
+
+        int lastPointIndex = packageAndClassName.lastIndexOf(".");
+        String packageName = packageAndClassName.substring(0, lastPointIndex);
+        String className = packageAndClassName.substring(lastPointIndex + 1);
+
+        List<ObjectDto> arguments = Arrays.stream(methodInvocation.getArgs()).map(TestRunAnalyser::createObjectDto).collect(Collectors.toList());
+        return TestRunDto.builder()
+                .packageName(packageName)
+                .className(className)
+                .methodName(methodName)
+                .arguments(arguments)
+                .result(createObjectDto(result))
+                .build();
+    }
+
+    private static ObjectDto createObjectDto(Object object) {
+        return ObjectDto.builder()
+                .typeName(object.getClass().toString())
+                .value(object.toString())
+                .build();
+    }
+}
