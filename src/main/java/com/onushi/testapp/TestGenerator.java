5659480b0a98da60445da8a7ad8a28cc3b092e8b
Ionut Breaz <ibreaz@yahoo.com>
2021-04-18 14:30:27 +0300

    replace most of the hardcoded parts in the test

--- src/main/java/com/onushi/testapp/TestGenerator.java
+++ src/main/java/com/onushi/testapp/TestGenerator.java
@@ -3,22 +3,63 @@ package com.onushi.testapp;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.springframework.stereotype.Component;
 
+import java.util.Locale;
+
 // TODO IB !!!! this should be written better later
 @Component
 public class TestGenerator {
 
-    // TODO IB !!!! this should be generated
     public void generate(ProceedingJoinPoint proceedingJoinPoint, Object result) {
+        String packageAndClassName = proceedingJoinPoint.getSignature().getDeclaringTypeName();
+        String methodName = proceedingJoinPoint.getSignature().getName();
+
+        int lastPointIndex = packageAndClassName.lastIndexOf(".");
+        String packageName = packageAndClassName.substring(0, lastPointIndex);
+        String className = packageAndClassName.substring(lastPointIndex + 1);
+
+        printBeginMarker();
+
+        printPackage(packageName);
+
+        printImports();
+
+        printClassAndTest(className, methodName, result);
+
+        printEndMarker();
+    }
+
+    private void printBeginMarker() {
         System.out.println();
         System.out.println("BEGIN GENERATED TEST =========");
         System.out.println();
-        System.out.println("class Service2Test {");
+    }
+
+    private void printPackage(String packageName) {
+        System.out.println("package " + packageName + ";");
+        System.out.println();
+    }
+
+    private void printImports() {
+        System.out.println("import org.junit.jupiter.api.Test;");
+        System.out.println();
+        System.out.println("import static org.junit.jupiter.api.Assertions.*;");
+        System.out.println();
+    }
+
+    private void printClassAndTest(String className, String methodName, Object result) {
+        String classNameVar = className.substring(0,1).toLowerCase(Locale.ROOT) + className.substring(1);
+
+        System.out.println("class " + className + "Test {");
         System.out.println("    @Test");
-        System.out.println("    void add() throws Exception {");
-        System.out.println("        Service2 service2 = new Service2();");
-        System.out.println("        assertEquals(service2.add(2, 3), 5);");
+        System.out.println("    void " + methodName + "() throws Exception {");
+        System.out.println("        " + className + " " + classNameVar + " = new " + className + "();");
+        // TODO IB !!!! this should be generated
+        System.out.println("        assertEquals(" + classNameVar + "." + methodName + "(2, 3), " + result + ");");
         System.out.println("    }");
         System.out.println("}");
+    }
+
+    private void printEndMarker() {
         System.out.println();
         System.out.println("END GENERATED TEST =========");
         System.out.println();
