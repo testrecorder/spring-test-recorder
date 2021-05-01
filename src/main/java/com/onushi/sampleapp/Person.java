21f37c4cf92934559c6236024b377f3a9b6de4fc
Ionut Breaz <ibreaz@yahoo.com>
2021-05-01 08:51:15 +0300

    add comments about how to get class information in case all members are private use lombok better

--- src/main/java/com/onushi/sampleapp/Person.java
+++ src/main/java/com/onushi/sampleapp/Person.java
@@ -1,27 +1,22 @@
 package com.onushi.sampleapp;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Person {
     private String firstName;
     private String lastName;
     private Date dateOfBirth;
 
-    public Person() {
-    }
-
     private Person(String firstName) {
         this.firstName = firstName;
-    }
-
-    public Person(String firstName, String lastName, Date dateOfBirth) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.dateOfBirth = dateOfBirth;
     }
 }
--- src/main/java/com/onushi/testrecording/aspect/RecordTest.java
+++ src/main/java/com/onushi/testrecording/aspect/RecordTest.java
@@ -5,7 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-// Unit tests will be generated for this method from runtime parameters and result
+// Unit tests will be generated for this method from runtime arguments and result
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RecordTest {
