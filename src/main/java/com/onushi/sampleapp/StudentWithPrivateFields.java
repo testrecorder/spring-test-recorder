a2312fef2677cf1898839dff42320a2769297e37
Ionut Breaz <ibreaz@yahoo.com>
2021-05-03 22:27:56 +0300

    test with static fields

--- src/main/java/com/onushi/sampleapp/StudentWithPrivateFields.java
+++ src/main/java/com/onushi/sampleapp/StudentWithPrivateFields.java
@@ -2,6 +2,8 @@ package com.onushi.sampleapp;
 
 public class StudentWithPrivateFields extends StudentWithPrivateField {
     private String lastName;
+    // TODO IB static fields are not handled yet
+    public static String middleName = "mn";
 
     public StudentWithPrivateFields(String firstName, String lastName) {
         this.firstName = firstName;
