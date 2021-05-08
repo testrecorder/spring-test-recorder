f78876a944ee8ce17d4bf8c86cbf12a718dfc256
Ionut Breaz <ibreaz@yahoo.com>
2021-05-08 08:33:13 +0300

    clean

--- src/main/java/com/onushi/sampleapp/StudentWithBuilder.java
+++ src/main/java/com/onushi/sampleapp/StudentWithBuilder.java
@@ -12,25 +12,7 @@ public class StudentWithBuilder {
     private String firstName;
     private String lastName;
     private int age;
-//    private boolean isOnline;
-//    private boolean registered;
-//    private boolean isab;
 
     private StudentWithBuilder() {
     }
-
-//    public StudentWithBuilder setOnline(boolean online) {
-//        isOnline = online;
-//        return this;
-//    }
-//
-//    public StudentWithBuilder setRegistered(boolean registered) {
-//        this.registered = registered;
-//        return this;
-//    }
-//
-//    public StudentWithBuilder setIsab(boolean isab) {
-//        this.isab = isab;
-//        return this;
-//    }
 }
