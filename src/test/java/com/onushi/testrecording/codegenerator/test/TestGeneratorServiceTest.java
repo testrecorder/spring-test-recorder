6b23dbed87253c29017c7932e0fa36647fe7893c
Ionut Breaz <ibreaz@yahoo.com>
2021-06-05 14:29:28 +0300

    add testCyclicDependenciesInResult

--- src/test/java/com/onushi/testrecording/codegenerator/test/TestGeneratorServiceTest.java
+++ src/test/java/com/onushi/testrecording/codegenerator/test/TestGeneratorServiceTest.java
@@ -1327,5 +1327,60 @@ class TestGeneratorServiceTest {
                 StringUtils.prepareForCompare(testString));
     }
 
+    @Test
+    void testCyclicDependenciesInResult() throws Exception {
+        // Arrange
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        CyclicParent cyclicParent = new CyclicParent();
+        CyclicChild cyclicChild = new CyclicChild();
+        cyclicChild.parent = cyclicParent;
+        cyclicChild.date = simpleDateFormat.parse("1980-01-02");
+        cyclicParent.id = 1;
+        cyclicParent.childList = Collections.singletonList(cyclicChild);
+
+        RecordedMethodRunInfo recordedMethodRunInfo = RecordedMethodRunInfo.builder()
+                .target(new SampleService())
+                .methodName("createCyclicObjects")
+                .arguments(Collections.emptyList())
+                .result(cyclicParent)
+                .dependencyMethodRuns(new ArrayList<>())
+                .build();
+        TestGenerator testGenerator = testGeneratorFactory.createTestGenerator(recordedMethodRunInfo);
+
+        // Act
+        String testString = testGeneratorService.generateTestCode(testGenerator);
+
+        // Assert
+        assertEquals(StringUtils.prepareForCompare("BEGIN GENERATED TEST =========\n" +
+                        "\n" +
+                        "package com.onushi.sampleapp.services;\n" +
+                        "\n" +
+                        "import org.junit.jupiter.api.Test;\n" +
+                        "import static org.junit.jupiter.api.Assertions.*;\n" +
+                        "import com.onushi.sampleapp.model.CyclicParent;\n" +
+                        "import java.text.SimpleDateFormat;\n" +
+                        "\n" +
+                        "class SampleServiceTest {\n" +
+                        "    //TODO rename the test to describe the use case\n" +
+                        "    //TODO refactor the generated code to make it easier to understand\n" +
+                        "    @Test\n" +
+                        "    void createCyclicObjects() throws Exception {\n" +
+                        "        // Arrange\n" +
+                        "        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(\"yyyy-MM-dd HH:mm:ss.SSS\");\n" +
+                        "        SampleService sampleService = new SampleService();\n" +
+                        "\n" +
+                        "        // Act\n" +
+                        "        CyclicParent result = sampleService.createCyclicObjects();\n" +
+                        "\n" +
+                        "        // Assert\n" +
+                        "        assertEquals(1, result.childList.size());\n" +
+                        "        assertEquals(simpleDateFormat.parse(\"1980-01-02 00:00:00.000\"), result.childList.get(0).date);\n" +
+                        "        assertEquals(1, result.id);\n" +
+                        "    }\n" +
+                        "}\n" +
+                        "\n" +
+                        "END GENERATED TEST ========="),
+                StringUtils.prepareForCompare(testString));
+    }
 
 }
