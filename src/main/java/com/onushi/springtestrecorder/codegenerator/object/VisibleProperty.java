6c013a68f72aaa1d374166dcc8a1f9020c157c29
Ionut Breaz <ibreaz@yahoo.com>
2021-06-17 19:27:20 +0300

    add initialValue, initialDependencies

--- src/main/java/com/onushi/springtestrecorder/codegenerator/object/VisibleProperty.java
+++ src/main/java/com/onushi/springtestrecorder/codegenerator/object/VisibleProperty.java
@@ -5,14 +5,16 @@ import lombok.Getter;
 
 import java.util.List;
 
+// TODO IB !!!! for all objects set initialValue, finalValue, initialDependencies and finalDependencies
 @Getter
 @Builder
 public class VisibleProperty {
     protected PropertySource propertySource;
-    // protected String initialValue = null;
+    protected PropertyValue initialValue;
     protected PropertyValue finalValue;
     // dependencies that not come from value, like the key of a map
     // protected List<ObjectInfo> initialDependencies = new ArrayList<>();
+    protected List<ObjectInfo> initialDependencies;
     protected List<ObjectInfo> finalDependencies;
     protected List<String> requiredImports;
     protected List<String> requiredHelperObjects;
