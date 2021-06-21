edfd348ea82a014fbf3226a327de943a8e9f8cb9
Ionut Breaz <ibreaz@yahoo.com>
2021-06-21 19:21:56 +0300

    Side Effects     write Equivalence method and test with ObjectInfoService_ObjectWithString

--- src/main/java/com/onushi/springtestrecorder/codegenerator/object/ObjectInfoService.java
+++ src/main/java/com/onushi/springtestrecorder/codegenerator/object/ObjectInfoService.java
@@ -1,17 +1,80 @@
 package com.onushi.springtestrecorder.codegenerator.object;
 
+import com.onushi.springtestrecorder.codegenerator.test.TestRecordingMoment;
+import lombok.NonNull;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
+// TODO IB !!!! use @NotNull in other places too
 // TODO IB !!!! continue here and recursively compare ObjectInfos
 // TODO IB !!!! avoid cyclic traversal
 // TODO IB !!!! avoid cyclic traversal when doing asserts too. but maybe it's solved from context already
 // TODO IB !!!! test cyclic
+// TODO IB !!!! make sure to have 100% coverage here
 @Service
 public class ObjectInfoService {
+    public boolean objectInfoEquivalent(ObjectInfo objectInfo1, @NonNull TestRecordingMoment testRecordingMoment1,
+                                        ObjectInfo objectInfo2, @NonNull TestRecordingMoment testRecordingMoment2) {
+        if (objectInfo1 == null && objectInfo2 == null) {
+            return true;
+        }
+        if (objectInfo1 == null || objectInfo2 == null) {
+            return false;
+        }
+        if (objectInfo1 == objectInfo2 && testRecordingMoment1 == testRecordingMoment2) {
+            return true;
+        }
+        List<VisibleProperty> visibleProperties1 = objectInfo1.getVisibleProperties().values().stream()
+                .filter(x -> (testRecordingMoment1 == TestRecordingMoment.FIRST_SNAPSHOT ? x.getFirstSnapshot() : x.getLastSnapshot()) != null)
+                .collect(Collectors.toList());
+        List<VisibleProperty> visibleProperties2 = objectInfo2.getVisibleProperties().values().stream()
+                .filter(x -> (testRecordingMoment2 == TestRecordingMoment.FIRST_SNAPSHOT ? x.getFirstSnapshot() : x.getLastSnapshot()) != null)
+                .collect(Collectors.toList());
+        if (visibleProperties1.size() != visibleProperties2.size()) {
+            return false;
+        }
+        for (VisibleProperty visibleProperty1 : visibleProperties1) {
+            PropertyValue propertyValue1 = (testRecordingMoment1 == TestRecordingMoment.FIRST_SNAPSHOT ?
+                    visibleProperty1.getFirstSnapshot().getValue() : visibleProperty1.getLastSnapshot().getValue());
+            VisibleProperty visibleProperty2 = visibleProperties2.stream()
+                    .filter(x -> x.getKey().equals(visibleProperty1.getKey()))
+                    .findFirst().orElse(null);
+            if (visibleProperty2 == null) {
+                return false;
+            }
+            PropertyValue propertyValue2 = (testRecordingMoment2 == TestRecordingMoment.FIRST_SNAPSHOT ?
+                    visibleProperty2.getFirstSnapshot().getValue() : visibleProperty2.getLastSnapshot().getValue());
+            if (!propertyValuesEquivalent(propertyValue1, testRecordingMoment1, propertyValue2, testRecordingMoment2)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean propertyValuesEquivalent(PropertyValue propertyValue1, TestRecordingMoment testRecordingMoment1,
+                                             PropertyValue propertyValue2, TestRecordingMoment testRecordingMoment2) {
+        if (propertyValue1.getString() != null) {
+            if (propertyValue2.getString() == null) {
+                return false;
+            } else {
+                return propertyValue1.getString().equals(propertyValue2.getString());
+            }
+        }
+
+        if (propertyValue1.getObjectInfo() != null) {
+            if (propertyValue2.getObjectInfo() == null) {
+                return false;
+            } else {
+                return objectInfoEquivalent(propertyValue1.getObjectInfo(), testRecordingMoment1, propertyValue2.getObjectInfo(), testRecordingMoment2);
+            }
+        }
+
+        return propertyValue2.getString() == null && propertyValue2.getObjectInfo() == null;
+    }
+
+
     /* TODO IB !!!! 1 reactivate
         public boolean objectInfoChangedDeep(ObjectInfo objectInfo) {
             if (objectInfo == null || objectInfo.getVisibleProperties() == null) {
---
+++ src/main/java/com/onushi/springtestrecorder/codegenerator/test/TestRecordingMoment.java
@@ -0,0 +1,6 @@
+package com.onushi.springtestrecorder.codegenerator.test;
+
+public enum TestRecordingMoment {
+    FIRST_SNAPSHOT,
+    LAST_SNAPSHOT
+}
